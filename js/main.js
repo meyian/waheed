28ee8e187a1b29b605bb86cdad946577283525de
Hassan M. Abudu <meyian@gmail.com>
2019-08-27 13:04:41 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*
+
+Stuff to do:
+* Get this into github
+* Clear out unneeded js files
+* Simply page. Everything in a vertical column
+* Get some testing in here.
+* Start plumbing
+
+*/
+
+$(function() {
+  // variables
+
+  // classes
+
+  // functions
+
+  function catastrophicFailure(strErrMsg) {}
+
+  function initHandlers() {}
+
+  function init() {
+    console.log("line 32. init");
+    initHandlers();
+  }
+
+  // script
+
+  init();
+});
